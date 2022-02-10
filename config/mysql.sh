5ec42027352cc2782b8832167ada4d870efc37f9
als698 <no@email.com>
2022-02-10 16:25:22 +0200

    mysql fix

--- config/mysql.sh
+++ config/mysql.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
+chmod 777 -R /db
 if [ ! -d "/run/mysqld" ]; then
   mkdir -p /run/mysqld
 fi
@@ -43,16 +44,16 @@ if [ ! -f "$tfile" ]; then
     return 1
 fi
 
-
 if [ -d /db/data/mysql ]; then
   echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: MySQL directory already present, skipping creation"
 else
   echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: creating database"
   mysql_install_db --user=nobody > /dev/null
   echo "USE mysql;" >> $tfile
-  echo "FLUSH PRIVILEGES;" >> $tfile
   echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" >> $tfile
-  echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" >> $tfile
+  echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '' WITH GRANT OPTION;" >> $tfile
+  echo "DROP DATABASE IF EXISTS test;" >> $tfile
+  echo "FLUSH PRIVILEGES;" >> $tfile
 fi
 
 if [ -d /db/data/${MYSQL_DATABASE} ]; then
@@ -61,21 +62,53 @@ else
   echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: $MYSQL_DATABASE not found, creating initial DBs"
   echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
   IMPORT_DB=1
-fi
-
-touch /db/import.sh
-chmod +x /db/import.sh
-if [ "$IMPORT_DB" = "1" ]; then
-  echo "echo date +'%D %T mysql [info]: importing /db/opencart.sql to $MYSQL_DATABASE'" >> /db/import.sh
-  echo "mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -f -D $MYSQL_DATABASE < /db/opencart.sql && return 0" >> /db/import.sh
+  cp /opencart.sql /db/opencart.sql
 fi
 
 echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: creating $MYSQL_USER"
-# echo "CREATE USER \`$MYSQL_USER\`@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+# echo "CREATE USER \`$MYSQL_USER\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> $tfile
 echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: grant privileges $MYSQL_USER to $MYSQL_DATABASE"
 echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO \`$MYSQL_USER\`@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO \`$MYSQL_USER\`@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
 
-/usr/bin/mysqld --user=nobody --bootstrap --verbose=0 < $tfile
+/usr/bin/mysqld --skip-networking &
+pid="$!"
+
+mysql=( mysql --protocol=socket --socket=/run/mysqld/mysqld.sock )
+
+for i in {30..0}; do
+    echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: MySQL init process in progress..."
+    if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
+        break
+    fi
+    
+    sleep 1
+done
+
+if [ "$i" = 0 ]; then
+    echo >&2 $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: MySQL init process failed."
+    exit 1
+else
+    echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: Database initiated [1/2]"
+fi
+
+if [ "$IMPORT_DB" = "1" ]; then
+    echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: importing /db/opencart.sql to $MYSQL_DATABASE"
+    echo "USE $MYSQL_DATABASE;" >> $tfile
+    echo "source /db/opencart.sql;" >> $tfile
+fi
+
+"${mysql[@]}" < $tfile
+
+if ! kill -s TERM "$pid" || ! wait "$pid"; then
+    echo >&2 $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: MySQL init process failed."
+    exit 1
+else
+    echo $(date '+%Y-%m-%d %H:%M:%S') "mysql [info]: Database initiated [2/2]"
+fi
+
 rm -f $tfile
 
-exec /usr/bin/mysqld --user=nobody --console
+echo $(date '+%Y-%m-%d %H:%M:%S') "STARTING DATABASE"
+
+exec /usr/bin/mysqld
